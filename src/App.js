e8e253a9fe44668c014122a270b3e92108a2fb18
dillon hoang <dillonh871@gmail.com>
2020-11-23 23:22:30 -0500

    assignment 9 changes

--- src/App.js
+++ src/App.js
@@ -1,72 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-// class RegularClass{}
-// class ComponentCLass extends Component{}
+import './App.css';
+import Fetcher from './components/Fetcher';
+import Hooks, {aFunc} from './components/Hooks';
+import ToggleButtons from './components/ToggleButtons';
 
-// const regularClassInstance = new RegularClass();
-// const componentCLassInstance = new Component();
+const publicURL = 'https://swe432tomcat.herokuapp.com';
+export const getLocationUrlData = () => {
+  return {
+      url:
+          process.env.NODE_ENV === 'production'?
+          publicURL
+          :`${window.location.origin}`,
+      hash: `${window.location.hash}`
+  };
+};
+export const servicePath ='/echo';
 
-// console.log('regularCLassInstance', regularClassInstance);
-// console.log('componentCLassInstance', componentCLassInstance);
-
-class App {
-    state = { string: false }; //In place of constructor
-
-    mySubmitHandler = (event) => {
-        event.preventDefault();
-        alert("You are saving " + this.state.string);
-    }
-    myChangeHandler = (event) => {
-        this.setState({ string: event.target.value });
-        //SAVE STRINGS
-    }
-    myChooseHandler = (event) => {
-        event.preventDefault();
-        //CHOOSE RANDOM
-    }
-    myClearHandler = (event) => {
-        event.preventDefault();
-        alert("You have cleared the dataset");
-        //CLEAR THE STORED
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Assignment 9</h1>
-                <div>
-                    <form onSubmit={this.mySubmitHandler}>
-                        <p>Enter string and click "save" to store it in the dataset</p>
-                        <input
-                            id="standard-string"
-                            label="String"
-                            type="text"
-                            onChange={this.myChangeHandler}
-                        />
-                        <input
-                            type='submit'
-                        />
-                    </form>
-                </div>
-                <div>
-                    <form onSubmit={this.myChooseHandler}>
-                        <p>Click here to choose a random string from the dataset</p>
-                        <input
-                            type='submit'
-                        />
-                    </form>
-                </div>
-                <div>
-                    <form onSubmit={this.myClearHandler}>
-                        <p>Click here to clear the dataset</p>
-                        <input
-                            type='submit'
-                        />
-                    </form>
-                </div>
-            </div>
-        )
-    }
+function App(props) {
+  const [weekDay, setWeekDay] = React.useState("Monday");
+  return (
+    <div style={{flexGrow: 1}}>
+      <Grid 
+      container
+      direction="column"
+      justify="center"
+      alignItems="stretch"
+      spacing={2}
+      >
+        <Grid item xs>
+          <Paper elevation={1}>
+            <Hooks name={aFunc().name}/>
+            </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper elevation={1}>
+            <Fetcher  value={weekDay} url={`${getLocationUrlData().url}${servicePath}`}/>
+            </Paper>
+          </Grid>
+        <Grid item xs>
+          <Paper elevation={1}>
+            <ToggleButtons value={weekDay} onChange ={setWeekDay}/>
+          </Paper>
+          </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
