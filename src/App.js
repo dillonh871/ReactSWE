94367cae08857b1cc6bb57b6aed38dece8b3b55f
dillon hoang <dillonh871@gmail.com>
2020-11-24 00:05:50 -0500

    assignment 9 changes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,74 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import React, { Component } from 'react';
+import {Component} from 'react';
 
-import './App.css';
-import Fetcher from './components/Fetcher';
-import Hooks, {aFunc} from './components/Hooks';
-import ToggleButtons from './components/ToggleButtons';
+
+// class RegularClass{}
+// class ComponentCLass extends Component{}
+
+// const regularClassInstance = new RegularClass();
+// const componentCLassInstance = new Component();
+
+// console.log('regularCLassInstance', regularClassInstance);
+// console.log('componentCLassInstance', componentCLassInstance);
 
 class App extends Component {
-  state = { string: false }; //In place of constructor
+    state = { string: false }; //In place of constructor
 
-  mySubmitHandler = (event) => {
-      event.preventDefault();
-      alert("You are saving " + this.state.string);
-  }
-  myChangeHandler = (event) => {
-      this.setState({ string: event.target.value });
-      //SAVE STRINGS
-  }
-  myChooseHandler = (event) => {
-      event.preventDefault();
-      //CHOOSE RANDOM
-  }
-  myClearHandler = (event) => {
-      event.preventDefault();
-      alert("You have cleared the dataset");
-      //CLEAR THE STORED
-  }
+    mySubmitHandler = (event) => {
+        event.preventDefault();
+        alert("You are saving " + this.state.string);
+    }
+    myChangeHandler = (event) => {
+        this.setState({ string: event.target.value });
+        //SAVE STRINGS
+    }
+    myChooseHandler = (event) => {
+        event.preventDefault();
+        //CHOOSE RANDOM
+    }
+    myClearHandler = (event) => {
+        event.preventDefault();
+        alert("You have cleared the dataset");
+        //CLEAR THE STORED
+    }
 
-  render() {
-      return (
-          <div>
-              <h1>Assignment 9</h1>
-              <div>
-                  <form onSubmit={this.mySubmitHandler}>
-                      <p>Enter string and click "save" to store it in the dataset</p>
-                      <input
-                          id="standard-string"
-                          label="String"
-                          type="text"
-                          onChange={this.myChangeHandler}
-                      />
-                      <input
-                          type='submit'
-                      />
-                  </form>
-              </div>
-              <div>
-                  <form onSubmit={this.myChooseHandler}>
-                      <p>Click here to choose a random string from the dataset</p>
-                      <input
-                          type='submit'
-                      />
-                  </form>
-              </div>
-              <div>
-                  <form onSubmit={this.myClearHandler}>
-                      <p>Click here to clear the dataset</p>
-                      <input
-                          type='submit'
-                      />
-                  </form>
-              </div>
-          </div>
-      )
-  }
+    render() {
+        return (
+            <div>
+                <h1>Assignment 9</h1>
+                <div>
+                    <form onSubmit={this.mySubmitHandler}>
+                        <p>Enter string and click "save" to store it in the dataset</p>
+                        <input
+                            id="standard-string"
+                            label="String"
+                            type="text"
+                            onChange={this.myChangeHandler}
+                        />
+                        <input
+                            type='submit'
+                        />
+                    </form>
+                </div>
+                <div>
+                    <form onSubmit={this.myChooseHandler}>
+                        <p>Click here to choose a random string from the dataset</p>
+                        <input
+                            type='submit'
+                        />
+                    </form>
+                </div>
+                <div>
+                    <form onSubmit={this.myClearHandler}>
+                        <p>Click here to clear the dataset</p>
+                        <input
+                            type='submit'
+                        />
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
